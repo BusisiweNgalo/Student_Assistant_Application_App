2a60115807b9cf859437ee14f113c37ec8bd78c4
TshepisoLaat <222057332@stud.cut.ac.za>
2026-05-19 19:53:51 +0200

    Create app_route.dart
    this file manages all navigation between screens in your Student Assistant App. It acts as a central traffic controller that tells Flutter which screen to show when navigation happens.

---
+++ lib/core/routes/app_route.dart
@@ -0,0 +1,8 @@
+class AppRoutes {
+  static const String login = '/login';
+  static const String signup = '/signup';
+  static const String studentHome = '/student/home';
+  static const String applicationForm = '/student/apply';
+  static const String applicationDetail = '/student/application';
+  static const String adminDashboard = '/admin/dashboard';
+}
