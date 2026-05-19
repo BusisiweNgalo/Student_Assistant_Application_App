e500be6ee920f80e9c3fce1b4b7bdd5aacc19354
Thakane0315 <annathakanerasego@gmail.com>
2026-05-19 16:43:38 +0200

    Add student information comment block
    Added a comment block with student information and question.

--- lib/provider/application_provider.dart
+++ lib/provider/application_provider.dart
@@ -1,3 +1,9 @@
+/**
+*Student Number: 218009030, 220049798, 220033640, 222057332, 221002961.
+*Student Name: TA RASEGO, LONWABO SIFUMBA, BN NGALO, TC LAAT, TC RADEBE.
+*Question: application_provider.
+*/
+
 import 'package:flutter/material.dart';
 import 'package:sa_apply/models/application_model.dart';
 import 'package:sa_apply/services/application_service.dart';
